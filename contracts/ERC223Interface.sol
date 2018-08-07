fd48169a09e259e2c7643816c89c0d3ea6face6e
Sergey Bushnyak <sergey.bushnyak@sigrlami.eu>
2018-08-07 13:50:35 +0300

    update coin contracts

---
+++ contracts/ERC223Interface.sol
@@ -0,0 +1,16 @@
+/**
+ * ERC223 additions to ERC20
+ *
+ * Interface wise is ERC20 + data paramenter to transfer and transferFrom.
+ */
+contract ERC223 is ERC20 {
+    function transfer(address to, uint value, bytes data) returns (bool ok);
+    function transferFrom(address from, address to, uint value, bytes data) returns (bool ok);
+}
+
+/**
+ *  Base class contracts willing to accept ERC223 token transfers must conform to.
+ */
+contract ERC223Receiver {
+  function tokenFallback(address _sender, address _origin, uint _value, bytes _data) returns (bool ok);
+}
--- contracts/UniversalCoin.sol
+++ contracts/UniversalCoin.sol
@@ -4,6 +4,69 @@ import "./BaseToken.sol";
 import "./ERC223Interface.sol";
 import "./ERC223ReceivingContract.sol";
 
+contract Standard223Token is ERC223, StandardToken {
+  //function that is called when a user or another contract wants to transfer funds
+  function transfer(address _to, uint _value, bytes _data) returns (bool success) {
+    //filtering if the target is a contract with bytecode inside it
+    if (!super.transfer(_to, _value)) throw; // do a normal token transfer
+    if (isContract(_to)) return contractFallback(msg.sender, _to, _value, _data);
+    return true;
+  }
+
+  function transferFrom(address _from, address _to, uint _value, bytes _data) returns (bool success) {
+    if (!super.transferFrom(_from, _to, _value)) throw; // do a normal token transfer
+    if (isContract(_to)) return contractFallback(_from, _to, _value, _data);
+    return true;
+  }
+
+  function transfer(address _to, uint _value) returns (bool success) {
+    return transfer(_to, _value, new bytes(0));
+  }
+
+  function transferFrom(address _from, address _to, uint _value) returns (bool success) {
+    return transferFrom(_from, _to, _value, new bytes(0));
+  }
+
+  //function that is called when transaction target is a contract
+  function contractFallback(address _origin, address _to, uint _value, bytes _data) private returns (bool success) {
+    ERC223Receiver reciever = ERC223Receiver(_to);
+    return reciever.tokenFallback(msg.sender, _origin, _value, _data);
+  }
+
+  //assemble the given address bytecode. If bytecode exists then the _addr is a contract.
+  function isContract(address _addr) private returns (bool is_contract) {
+    // retrieve the size of the code on target address, this needs assembly
+    uint length;
+    assembly { length := extcodesize(_addr) }
+    return length > 0;
+  }
+}
+
+
+/**
+ * @title Burnable Token
+ * @dev Token that can be irreversibly burned (destroyed).
+ */
+contract BurnableToken is Standard223Token {
+
+    event Burn(address indexed burner, uint256 value);
+
+    /**
+     * @dev Burns a specific amount of tokens.
+     * @param _value The amount of token to be burned.
+     */
+    function burn(uint256 _value) public {
+        require(_value <= balances[msg.sender]);
+        // no need to require value <= totalSupply, since that would imply the
+        // sender's balance is greater than the totalSupply, which *should* be an assertion failure
+
+        address burner = msg.sender;
+        balances[burner] = balances[burner].sub(_value);
+        totalSupply = totalSupply.sub(_value);
+        emit Burn(burner, _value);
+    }
+}
+
 contract UniversalCoin is ERC223Interface, BaseToken {
   // Publicly listed name
     string public name = "UNIVERSAL COIN";
